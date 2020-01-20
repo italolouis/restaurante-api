75bbd573d444b8edf54b9a333d4244c0eeddd6c2
italo.louis <italo.louis@hotmail.com>
2020-01-20 07:35:47 -0300

    Ajustes

--- src/main/java/br/com/restaurante/controller/PedidoController.java
+++ src/main/java/br/com/restaurante/controller/PedidoController.java
@@ -1,5 +1,7 @@
 package br.com.restaurante.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -9,11 +11,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.HttpStatus;
-
 import br.com.restaurante.model.Pedido;
 import br.com.restaurante.service.PedidoService;
 
@@ -29,6 +31,13 @@ public class PedidoController {
 	public ResponseEntity<Pedido> adicionarItem(@Valid @RequestBody Pedido pedido, HttpServletResponse response) {
 		Pedido pedidoSalvo = pedidoService.criarPedido(pedido);
 		return ResponseEntity.status(HttpStatus.CREATED).body(pedidoSalvo);
+	}
+	
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> getPedidos() {
+		List<Pedido> pedidos = pedidoService.listarPedidos();
+		return !pedidos.isEmpty() ? ResponseEntity.ok(pedidos) : ResponseEntity.notFound().build();
 	}
 	
 	
--- src/main/java/br/com/restaurante/service/PedidoService.java
+++ src/main/java/br/com/restaurante/service/PedidoService.java
@@ -1,5 +1,7 @@
 package br.com.restaurante.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,10 @@ public class PedidoService {
 		//Ao salvar o pedido atualizar os itens
 		itemRepository.updateItens(pedidoSalvo.getId());
 		return pedidoSalvo;
+	}
+	
+	public List<Pedido> listarPedidos(){
+		return pedidoRepository.findAll();
 	}
 	
 }
